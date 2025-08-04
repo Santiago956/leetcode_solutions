c8fdbd0b5ce6f8282e15dc1a7361e2cb33868639
Pedro Paulo <163353883+Santiago956@users.noreply.github.com>
2025-08-04 15:44:34 -0300

    Time: 548 ms (5.05%) | Memory: 0B (100.00%) - LeetSync

---
+++ medium/185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,12 @@
+-- Write your PostgreSQL query statement below
+SELECT d.name AS "Department",
+    e.name AS "Employee",
+    e.salary AS "Salary"
+FROM Employee AS e
+JOIN Department d ON e.departmentId = d.id
+WHERE (
+    SELECT COUNT(DISTINCT salary)
+    FROM Employee e2
+    WHERE e2.departmentId = e.departmentId AND e2.salary >= e.salary
+) <= 3
+ORDER BY d.name, e.salary DESC;
