c19bc4e669a6eda996d1194b6f6e89c7f81b1510
Pedro Paulo <163353883+Santiago956@users.noreply.github.com>
2025-09-22 11:58:35 -0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.68%) - LeetSync

--- medium/3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
+++ medium/3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
         freq = {}
-        maxfreq = 0
         qnt = 0
         for num in nums:
             if num not in freq:
@@ -9,10 +8,8 @@ class Solution:
             elif num in freq:
                 freq[num] += 1
         
-        for number in freq.keys():
-            if freq[number] > maxfreq:
-                maxfreq = freq[number]
-        
+        maxfreq = max(freq.values())
+
         for frequency in freq.values():
             if frequency == maxfreq:
                 qnt += frequency
